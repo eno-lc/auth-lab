1480e7269e892ae2324baae869e2d51826580da4
eno-lc <eh52370@ubt-uni.net>
2023-05-23 16:47:32 +0200

    added another controller for authenticated user and updated the security config to allow anyone in register and login but authenticated only in secured

---
+++ src/main/java/com/jwt/auth/controller/SecuredController.java
@@ -0,0 +1,17 @@
+package com.jwt.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/secured")
+public class SecuredController {
+
+    @GetMapping
+    public ResponseEntity<String> secured(){
+        return ResponseEntity.ok("Secured");
+    }
+
+}
