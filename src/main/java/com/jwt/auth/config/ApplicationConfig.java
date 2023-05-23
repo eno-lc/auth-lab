dbaf0fc4142e417f22149443175d613cd99763da
eno-lc <eh52370@ubt-uni.net>
2023-05-23 16:01:19 +0200

    adding the security configuration, comments for authentication filter, authentication manager bean, authentication provider bean, password encoder bean

--- src/main/java/com/jwt/auth/config/ApplicationConfig.java
+++ src/main/java/com/jwt/auth/config/ApplicationConfig.java
@@ -4,9 +4,15 @@ import com.jwt.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -25,4 +31,21 @@ public class ApplicationConfig {
         };
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/jwt/auth/config/JwtAuthenticationFilter.java
+++ src/main/java/com/jwt/auth/config/JwtAuthenticationFilter.java
@@ -26,27 +26,27 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-        final String authorizationHeader = request.getHeader("Authorization");
-        final String jwtToken;
-        final String userEmail;
+        final String authorizationHeader = request.getHeader("Authorization"); // this is the header that contains the jwt token
+        final String jwtToken; // this is the jwt token
+        final String userEmail; // this is the user email
 
-        if(authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")){
-            filterChain.doFilter(request, response);
-            return;
+        if(authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")){ // if the header is null or does not start with "Bearer "
+            filterChain.doFilter(request, response); // then we just pass the request and response to the next filter
+            return; // and return
         }
 
-        jwtToken = authorizationHeader.substring(7);
+        jwtToken = authorizationHeader.substring(7); // if the header is not null and starts with "Bearer " then we extract the jwt token
 
-        userEmail = jwtService.extractUserEmail(jwtToken);
+        userEmail = jwtService.extractUserEmail(jwtToken); // we extract the user email from the jwt token
 
-        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
-            UserDetails user = this.userDetailsService.loadUserByUsername(userEmail);
-            if(jwtService.isTokenValid(jwtToken, user)){
-                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){ // if the user email is not null and the authentication context is null
+            UserDetails user = this.userDetailsService.loadUserByUsername(userEmail); // we load the user details from the user email
+            if(jwtService.isTokenValid(jwtToken, user)){ // if the jwt token is valid
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities()); // we create an authentication token with the user details which we use to authenticate the user
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request)); // we set the details of the authentication token to the details of the request
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken); // we set the authentication token to the authentication context
             }
         }
-        filterChain.doFilter(request, response);
+        filterChain.doFilter(request, response); // we pass the request and response to the next filter
     }
 }
