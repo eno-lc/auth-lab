76dbdd8ee55f7e41dd29355c3adf9952fc6f8af5
eno-lc <eh52370@ubt-uni.net>
2023-05-23 16:17:43 +0200

    implementing registration logic for our authentication service

--- src/main/java/com/jwt/auth/service/AuthenticationService.java
+++ src/main/java/com/jwt/auth/service/AuthenticationService.java
@@ -1,18 +1,40 @@
 package com.jwt.auth.service;
 
+import com.jwt.auth.config.JwtService;
 import com.jwt.auth.dto.AuthenticationRequest;
 import com.jwt.auth.dto.AuthenticationResponse;
 import com.jwt.auth.dto.RegistrationRequest;
+import com.jwt.auth.entity.Role;
+import com.jwt.auth.entity.User;
+import com.jwt.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public AuthenticationResponse register(RegistrationRequest registrationRequest) {
-        return null;
+        User user = User.builder()
+                .firstName(registrationRequest.getFirstName())
+                .lastName(registrationRequest.getLastName())
+                .email(registrationRequest.getEmail())
+                .password(passwordEncoder.encode(registrationRequest.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+
+        String jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
