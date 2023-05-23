189cf1a7f538c9954e3e8a57c3fe34ae8f809764
eno-lc <eh52370@ubt-uni.net>
2023-05-23 20:50:20 +0200

    setting up the logout configuration, creating the logout handler implementation

---
+++ src/main/java/com/jwt/auth/config/LogoutService.java
@@ -0,0 +1,35 @@
+package com.jwt.auth.config;
+
+import com.jwt.auth.repository.TokenRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService implements LogoutHandler {
+
+    private final TokenRepository tokenRepository;
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        final String authorizationHeader = request.getHeader("Authorization"); // this is the header that contains the jwt token
+        final String jwtToken; // this is the jwt token
+
+        if(authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")){ // if the header is null or does not start with "Bearer "
+            return; // and return
+        }
+        jwtToken = authorizationHeader.substring(7); // if the header is not null and starts with "Bearer " then we extract the jwt token
+        var storedToken = tokenRepository.findByToken(jwtToken)
+                .orElse(null);
+
+        if(storedToken != null){
+            storedToken.setExpired(true);
+            storedToken.setRevoked(true);
+            tokenRepository.save(storedToken);
+        }
+    }
+}
--- src/main/java/com/jwt/auth/config/SecurityConfiguration.java
+++ src/main/java/com/jwt/auth/config/SecurityConfiguration.java
@@ -8,8 +8,10 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +20,7 @@ public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter; // we use JWT authentication filter to authenticate the user
     private final AuthenticationProvider authenticationProvider; // we use authentication provider to authenticate the user
+    private final LogoutHandler logoutHandler; // we use logout service to logout the user
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { // we use security filter chain to configure the security
@@ -33,8 +36,11 @@ public class SecurityConfiguration {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // we use stateless session because we are using JWT and we don't need to store the session because we are not using cookies
                 .and()
                 .authenticationProvider(authenticationProvider) // we use authentication provider to authenticate the user
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // we add the JWT authentication filter before the username password authentication filter because we want to authenticate the user before the username password authentication filter
-
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) // we add the JWT authentication filter before the username password authentication filter because we want to authenticate the user before the username password authentication filter
+                .logout() // we logout
+                .logoutUrl("/api/v1/auth/logout") // we log out from the url /api/v1/auth/logout
+                .addLogoutHandler(logoutHandler) // we add logout handler the work that we want to do when the user logout
+                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext()); // we clear the security context when the user logout
         return http.build();
     }
 
