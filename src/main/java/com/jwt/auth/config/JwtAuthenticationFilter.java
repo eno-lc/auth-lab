4ba6bd62e06da291ef8ced76ab855496a206470c
eno-lc <eh52370@ubt-uni.net>
2023-05-23 14:27:21 +0200

    adding the extra filter of jwt that is going to be triggered oncePerRequest and fetching the jwt token from the servlet request

---
+++ src/main/java/com/jwt/auth/config/JwtAuthenticationFilter.java
@@ -0,0 +1,31 @@
+package com.jwt.auth.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        final String authorizationHeader = request.getHeader("Authorization");
+        final String jwtToken;
+
+        if(authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwtToken = authorizationHeader.substring(7);
+
+    }
+}
