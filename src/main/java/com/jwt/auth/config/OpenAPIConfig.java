c4f338b5bc07965d1fc84fe2fe2c0911861e652b
eno-lc <eh52370@ubt-uni.net>
2023-05-25 11:44:39 +0200

    adding openapi configuration, try to test secured apis doesnt work

---
+++ src/main/java/com/jwt/auth/config/OpenAPIConfig.java
@@ -0,0 +1,41 @@
+package com.jwt.auth.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Enis",
+                        email = "enis.halilaj@evonem.com"
+                ),
+                description = "OpenAPI Documentation for our application",
+                title = "OpenAPI Documentation",
+                version = "1.0.0",
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                ),
+                termsOfService = "Terms of Service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local Environment",
+                        url = "http://localhost:8080"
+
+                ),
+                @Server(
+                        description = "Production Environment",
+                        url = "https://evonem.com"
+
+                )
+        }
+)
+public class OpenAPIConfig {
+}
